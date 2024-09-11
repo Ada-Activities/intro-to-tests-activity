9e8f92e165ab704d2b2d9c1ea55b7a1c3f9a9f27
Adrian Prado <adrian.prado@adadevelopersacademy.org>
2024-09-10 19:25:45 -0500

    Added solution

--- tests/test_count_a_letter.py
+++ tests/test_count_a_letter.py
@@ -1,19 +1,99 @@
 from main import count_a_letter
 import pytest
 
-def test_demo_one():
-    num_1 = 8
-    num_2 = 9
+def test_non_alpha_letter_returns_none():
+    test_sentence = "hello world"
+    test_letter = "3"
 
-    result = num_1 + num_2
+    result = count_a_letter(test_sentence, test_letter)
 
-    assert result == 17
+    assert result is None
 
-def test_demo_two():
-    num_1 = 18
-    num_2 = 24
+def test_empty_string_sentence_returns_none():
+    test_sentence = ""
+    test_letter = "e"
 
-    result = num_1 + num_2
+    result = count_a_letter(test_sentence, test_letter)
 
-    assert result == 42
-# Delete the demo tests and add your tests here 
+    assert result is None
+
+def test_empty_list_sentence_returns_none():
+    test_sentence = []
+    test_letter = "e"
+
+    result = count_a_letter(test_sentence, test_letter)
+
+    assert result is None
+
+def test_string_with_no_matches_returns_0():
+    test_sentence = "hello world"
+    test_letter = "q"
+
+    result = count_a_letter(test_sentence, test_letter)
+
+    assert result == 0
+
+def test_list_with_no_matches_returns_0():
+    test_sentence = list("hello world")
+    test_letter = "q"
+
+    result = count_a_letter(test_sentence, test_letter)
+
+    assert result == 0
+
+def test_string_with_one_match_returns_1():
+    test_sentence = "hello world"
+    test_letter = "w"
+
+    result = count_a_letter(test_sentence, test_letter)
+
+    assert result == 1
+
+def test_list_with_one_match_returns_1():
+    test_sentence = list("hello world")
+    test_letter = "w"
+
+    result = count_a_letter(test_sentence, test_letter)
+
+    assert result == 1
+
+def test_string_with_multiple_matches_returns_count():
+    test_sentence = "hello world"
+    test_letter = "l"
+
+    result = count_a_letter(test_sentence, test_letter)
+
+    assert result == 3
+
+def test_list_with_multiple_matches_returns_count():
+    test_sentence = list("hello world")
+    test_letter = "l"
+
+    result = count_a_letter(test_sentence, test_letter)
+
+    assert result == 3
+
+def test_string_with_substring_returns_0():
+    test_sentence = "hello world"
+    test_letter = "ello"
+
+    result = count_a_letter(test_sentence, test_letter)
+
+    assert result == 0
+
+def test_list_with_substring_returns_0():
+    test_sentence = list("hello world")
+    test_letter = "ello"
+
+    result = count_a_letter(test_sentence, test_letter)
+
+    assert result == 0
+
+# bonus
+def test_non_string_letter_reports_error():
+    test_sentence = "hello world"
+    test_letter = 3
+
+    with pytest.raises(AttributeError):
+        count_a_letter(test_sentence, test_letter)
+
