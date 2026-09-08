b563d9fdee82999a3182331c25ca5a31b543693c
V <valeriya.i.ten@gmail.com>
2026-09-08 13:06:42 -0700

    Three unit tests added

--- tests/test_count_a_letter.py
+++ tests/test_count_a_letter.py
@@ -1,19 +1,36 @@
 from main import count_a_letter
 import pytest
 
-def test_demo_one():
-    num_1 = 8
-    num_2 = 9
+def test_check_correct_count():
+    sentence = "This is a sentence"
+    letter = "a"
+    count = count_a_letter(sentence,letter)
+    assert count == 1
 
-    result = num_1 + num_2
+def test_non_alphabetic_input_in_string():
+    sentence = "This is a sentence"
+    letter = "10"
+    count = count_a_letter(sentence,letter)
+    assert count is None
 
-    assert result == 17
+def test_input_empty_strings():
+    sentence = ""
+    letter = ""
+    count = count_a_letter(sentence,letter)
+    assert count is None
+    
 
-def test_demo_two():
-    num_1 = 18
-    num_2 = 24
-
-    result = num_1 + num_2
-
-    assert result == 42
 # Delete the demo tests and add your tests here 
+
+# def count_a_letter(sentence, letter):
+#     if not letter.isalpha():
+#         return None
+#     if not sentence:
+#         return None
+    
+#     count = 0
+#     for char in sentence:
+#         if char == letter:
+#             count +=1
+    
+#     return count
